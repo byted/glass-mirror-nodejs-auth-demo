73d064a43899b51e4822075b9a35d4d03b633f96
byted <stefan.selent@mailbox.org>
2015-06-13 19:17:59 +0200

    changes2

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var oauth2Client = new OAuth2Client(process.env.MIRROR_DEMO_CLIENT_ID,
 var app = express();
 
 // all environments
-app.set('port', 8081);
+app.set('port', process.env.PORT);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -42,11 +42,12 @@ var gotToken = function () {
                 return;
             }
             console.log('mirror client', client);
-            listTimeline(client, failure, success);
-            insertHello(client, failure, success);
-            insertContact(client, failure, success);
-            insertLocation(client, failure, success);
-            getLocation(client, failure, success);
+            // listTimeline(client, failure, success);
+            // insertHello(client, failure, success);
+            //insertContact(client, failure, success);
+            // insertLocation(client, failure, success);
+            // getLocation(client, failure, success);
+            getContact(client, failure, success);
         });
 };
 
@@ -56,7 +57,8 @@ var insertHello = function (client, errorCallback, successCallback) {
         .mirror.timeline.insert(
         {
             "text": "Hello world",
-            "callbackUrl": "https://mirrornotifications.appspot.com/forward?url=http://localhost:8081/reply",
+            "html": "<article><section><img height='100%' src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Cheese_crust_pizza.jpg/1920px-Cheese_crust_pizza.jpg'></section></article>",
+            "callbackUrl": "https://mirrornotifications.appspot.com/forward?url=http://localhost:8080/reply",
             "menuItems": [
                 {"action": "REPLY"},
                 {"action": "DELETE"}
@@ -78,7 +80,7 @@ var insertLocation = function (client, errorCallback, successCallback) {
         .mirror.timeline.insert(
         {
             "text": "Let's meet at the Hacker Dojo!",
-            "callbackUrl": "https://mirrornotifications.appspot.com/forward?url=http://localhost:8081/reply",
+            "callbackUrl": "https://mirrornotifications.appspot.com/forward?url=http://localhost:8080/reply",
             "location": {
                 "kind": "mirror#location",
                 "latitude": 37.4028344,
@@ -103,23 +105,21 @@ var insertLocation = function (client, errorCallback, successCallback) {
 };
 
 //inserts a contact and icon for that contact
-var insertContact = function (client, errorCallback, successCallback) {
+var insertContact = function (contactObj, client, errorCallback, successCallback) {
     client
-        .mirror.contacts.insert(
-        {
-            "id": "emil10001",
-            "displayName": "emil10001",
-             "imageUrls": [
-            "https://secure.gravatar.com/avatar/bc6e3312f288a4d00ba25500a2c8f6d9.png"
-            ],
-            "priority": 7,
-            "acceptCommands": [
-                {"type": "REPLY"},
-                {"type": "POST_AN_UPDATE"},
-                {"type": "TAKE_A_NOTE"}
-            ]
-        }
-    )
+        .mirror.contacts.insert(contactObj)
+        .withAuthClient(oauth2Client)
+        .execute(function (err, data) {
+            if (!!err)
+                errorCallback(err);
+            else
+                successCallback(data);
+        });
+};
+
+var getContact = function (client, errorCallback, successCallback) {
+    client
+        .mirror.contacts.get({"id": "emil10001"})
         .withAuthClient(oauth2Client)
         .execute(function (err, data) {
             if (!!err)
@@ -169,6 +169,22 @@ var grabToken = function (code, errorCallback, successCallback) {
     });
 };
 
+var authenticate = function(res) {
+    // generates a url that allows offline access and asks permissions
+    // for Mirror API scope.
+    
+    var scopes = [
+      'https://www.googleapis.com/auth/glass.timeline',
+      'https://www.googleapis.com/auth/glass.location'
+    ];
+    
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes.join(" ")
+    });
+    res.redirect(url);
+}
+
 app.get('/', function (req, res) {
     if (!oauth2Client.credentials) {
         // generates a url that allows offline access and asks permissions
@@ -198,13 +214,38 @@ app.get('/oauth2callback', function (req, res) {
     });
 });
 app.post('/reply', function(req, res){
-    console.log('replied',req);
+    console.log('replied',req.body);
     res.end();
 });
 app.post('/location', function(req, res){
     console.log('location',req);
     res.end();
 });
+app.post('/contact', function(req, res) {
+    if (!oauth2Client.credentials) {
+        authenticate(res);
+    } else {
+        console.log('send contact');
+        googleapis
+            .discover('mirror', 'v1')
+            .execute(function (err, client) {
+                if (!!err) {
+                    failure();
+                    return;
+                }
+                insertContact(req.body, client, function(data) {
+                    res.end(JSON.stringify(data));
+                    failure(data);
+                }, function(data) {
+                    console.log("added contact:");
+                    res.end(JSON.stringify(data));
+                    success(data)
+                });
+            });
+    }
+});
+
+app.post
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
